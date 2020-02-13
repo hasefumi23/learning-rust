a0e53137881b5048ec0a39802836df14fbe2156a
hasefumi23 <hasefumi23@gmail.com>
2020-02-14 00:36:08 +0900

    [from now] 2020/02/14 00:36:08
    diff --git a/README.md b/README.md
    index 254c94d..26805b8 100644
    --- a/README.md
    +++ b/README.md
    @@ -1,5 +1,7 @@
     # learning-rust
    
     - https://doc.rust-jp.rs/book/second-edition/
    -- Cargo の doc
    +- Cargo
       - https://doc.rust-lang.org/cargo/
    +- from here
    +  - https://doc.rust-jp.rs/book/second-edition/ch04-01-what-is-ownership.html
    diff --git a/variables/src/main.rs b/variables/src/main.rs
    index 0e11042..9126a69 100644
    --- a/variables/src/main.rs
    +++ b/variables/src/main.rs
    @@ -1,6 +1,70 @@
     fn main() {
    -    for number in (1..4).rev() {
    -        println!("{}", number);
    +    let reference_to_nothing = dangle();
    +    let user1 = User {
    +        email: String::from("someone"),
    +        username: String::from("someuser"),
    +        active: true,
    +        sign_in_count: 1,
    +    };
    +    let user2 = User {
    +        email: String::from("mail"),
    +        username: String::from("username"),
    +        ..user1
    +    };
    +}
    +
    +fn build_user(email: String, username: String) -> User {
    +    User {
    +        email,
    +        username,
    +        active: true,
    +        sign_in_count: 1,
    +    }
    +}
    +
    +struct User {
    +    username: String,
    +    email: String,
    +    sign_in_count: u64,
    +    active: bool,
    +}
    +
    +fn first_word(s: &String) -> &str {
    +    let bytes = s.as_bytes();
    +    for (i, &item) in bytes.iter().enumerate() {
    +        if item == b' ' {
    +            return &s[..i];
    +        }
         }
    -    println!("L!!");
    +    &s[..]
    +}
    +
    +fn dangle() -> String {
    +    let s = String::from("hello");
    +    s
    +}
    +
    +fn change(some_string: &mut String) {
    +    some_string.push_str(", world");
    +}
    +
    +fn calculate_length(s: &String) -> usize {
    +    s.len()
    +}
    +
    +fn gives_ownership() -> String {
    +    let some_string = String::from("hello");
    +    some_string
    +}
    +
    +fn takes_and_gives_back(a_string: String) -> String {
    +    a_string
    +}
    +
    +fn takes_ownership(some_string: String) {
    +    println!("{}", some_string);
    +}
    +
    +fn makes_copy(some_integer: i32) {
    +    println!("{}", some_integer);
     }

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # learning-rust
 
 - https://doc.rust-jp.rs/book/second-edition/
-- Cargo の doc
+- Cargo
   - https://doc.rust-lang.org/cargo/
+- from here
+  - https://doc.rust-jp.rs/book/second-edition/ch04-01-what-is-ownership.html
--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,70 @@
 fn main() {
-    for number in (1..4).rev() {
-        println!("{}", number);
+    let reference_to_nothing = dangle();
+    let user1 = User {
+        email: String::from("someone"),
+        username: String::from("someuser"),
+        active: true,
+        sign_in_count: 1,
+    };
+    let user2 = User {
+        email: String::from("mail"),
+        username: String::from("username"),
+        ..user1
+    };
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
     }
-    println!("L!!");
+}
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    &s[..]
+}
+
+fn dangle() -> String {
+    let s = String::from("hello");
+    s
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
 }
