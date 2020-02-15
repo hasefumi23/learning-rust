5117fbe39d0eede6ab8b21889121902f03c475d9
hasefumi23 <hasefumi23@gmail.com>
2020-02-15 15:35:32 +0900

    [from now] 2020/02/15 15:35:31
    diff --git a/collection/.gitignore b/collection/.gitignore
    new file mode 100644
    index 0000000..f2e972d
    --- /dev/null
    +++ b/collection/.gitignore
    @@ -0,0 +1,6 @@
    +# Generated by Cargo
    +# will have compiled files and executables
    +/target/
    +
    +# These are backup files generated by rustfmt
    +**/*.rs.bk
    diff --git a/collection/Cargo.lock b/collection/Cargo.lock
    new file mode 100644
    index 0000000..eb12c89
    --- /dev/null
    +++ b/collection/Cargo.lock
    @@ -0,0 +1,5 @@
    +# This file is automatically @generated by Cargo.
    +# It is not intended for manual editing.
    +[[package]]
    +name = "collection"
    +version = "0.1.0"
    diff --git a/collection/Cargo.toml b/collection/Cargo.toml
    new file mode 100644
    index 0000000..14507cd
    --- /dev/null
    +++ b/collection/Cargo.toml
    @@ -0,0 +1,9 @@
    +[package]
    +name = "collection"
    +version = "0.1.0"
    +authors = ["hasefumi23 <hasefumi23@gmail.com>"]
    +edition = "2018"
    +
    +# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
    +
    +[dependencies]
    diff --git a/collection/src/main.rs b/collection/src/main.rs
    new file mode 100644
    index 0000000..e7a11a9
    --- /dev/null
    +++ b/collection/src/main.rs
    @@ -0,0 +1,3 @@
    +fn main() {
    +    println!("Hello, world!");
    +}
    diff --git a/communicator/src/main.rs b/communicator/src/main.rs
    index bb9a64b..d285225 100644
    --- a/communicator/src/main.rs
    +++ b/communicator/src/main.rs
    @@ -3,3 +3,13 @@ extern crate communicator;
     fn main() {
         communicator::client::connect();
     }
    +
    +#[cfg(test)]
    +mod tests {
    +    use super::communicator::client;
    +
    +    #[test]
    +    fn it_works() {
    +        client::connect();
    +    }
    +}
    diff --git a/try_me/Cargo.lock b/try_me/Cargo.lock
    new file mode 100644
    index 0000000..a233688
    --- /dev/null
    +++ b/try_me/Cargo.lock
    @@ -0,0 +1,5 @@
    +# This file is automatically @generated by Cargo.
    +# It is not intended for manual editing.
    +[[package]]
    +name = "try_me"
    +version = "0.1.0"
    diff --git a/try_me/main.rs b/try_me/main.rs
    new file mode 100644
    index 0000000..e69de29
    diff --git a/try_me/src/lib.rs b/try_me/src/lib.rs
    index 31e1bb2..067acff 100644
    --- a/try_me/src/lib.rs
    +++ b/try_me/src/lib.rs
    @@ -1,7 +1,43 @@
    -#[cfg(test)]
    -mod tests {
    -    #[test]
    -    fn it_works() {
    -        assert_eq!(2 + 2, 4);
    +mod outermost {
    +    pub fn middle_function() {}
    +
    +    pub fn middle_secret_function() {}
    +
    +    pub mod inside {
    +        pub fn inner_function() {}
    +
    +        pub fn secret_function() {}
         }
     }
    +
    +fn try_me() {
    +    outermost::middle_function();
    +    outermost::middle_secret_function();
    +    outermost::inside::inner_function();
    +    outermost::inside::secret_function();
    +}
    +
    +// #[cfg(test)]
    +// mod tests {
    +//     #[test]
    +//     fn it_works() {
    +//         assert_eq!(2 + 2, 4);
    +//     }
    +// }
    +
    +pub mod a {
    +    pub mod series {
    +        pub mod of {
    +            pub fn nested_modules() {
    +                println!("nested_modules is called");
    +            }
    +        }
    +    }
    +}
    +
    +use a::series::of;
    +
    +fn main() {
    +    a::series::of::nested_modules();
    +    of::nested_modules();
    +}

--- communicator/src/main.rs
+++ communicator/src/main.rs
@@ -3,3 +3,13 @@ extern crate communicator;
 fn main() {
     communicator::client::connect();
 }
+
+#[cfg(test)]
+mod tests {
+    use super::communicator::client;
+
+    #[test]
+    fn it_works() {
+        client::connect();
+    }
+}
--- try_me/src/lib.rs
+++ try_me/src/lib.rs
@@ -1,7 +1,43 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+mod outermost {
+    pub fn middle_function() {}
+
+    pub fn middle_secret_function() {}
+
+    pub mod inside {
+        pub fn inner_function() {}
+
+        pub fn secret_function() {}
     }
+}
+
+fn try_me() {
+    outermost::middle_function();
+    outermost::middle_secret_function();
+    outermost::inside::inner_function();
+    outermost::inside::secret_function();
+}
+
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(2 + 2, 4);
+//     }
+// }
+
+pub mod a {
+    pub mod series {
+        pub mod of {
+            pub fn nested_modules() {
+                println!("nested_modules is called");
+            }
+        }
+    }
+}
+
+use a::series::of;
+
+fn main() {
+    a::series::of::nested_modules();
+    of::nested_modules();
 }
