30c3630480b2bf26dee356ebe56894445907cbde
hasefumi23 <hasefumi23@gmail.com>
2020-02-15 17:28:18 +0900

    [from now] 2020/02/15 17:28:18
    diff --git a/collection/src/main.rs b/collection/src/main.rs
    index e7a11a9..d7d1840 100644
    --- a/collection/src/main.rs
    +++ b/collection/src/main.rs
    @@ -1,3 +1,56 @@
     fn main() {
    -    println!("Hello, world!");
    +    let mut s = String::new();
    +    let data = "inital contents";
    +    let s = data.to_string();
    +    println!("{}", s);
    +    let s = "inital contents".to_string();
    +    println!("{}", s);
    +    let s = String::from("inital contents");
    +    println!("{}", s);
    +
    +    let mut s = String::from("foo");
    +    println!("{}", s);
    +    s.push_str("bar");
    +    println!("{}", s);
    +
    +    let mut s1 = String::from("foo");
    +    let s2 = "bar";
    +    s1.push_str(s2);
    +    println!("s2 is {}", s2);
    +
    +    let s1 = String::from("tic");
    +    let s2 = String::from("tac");
    +    let s3 = String::from("toe");
    +    let s = format!("{}-{}-{}", s1, s2, s3);
    +    println!("s is {}", s);
    +
    +    for c in "नमस्ते".chars() {
    +        println!("{}", c);
    +    }
    +
    +    use std::collections::HashMap;
    +
    +    let mut scores = HashMap::new();
    +    scores.insert(String::from("Blue"), 10);
    +    scores.insert(String::from("Yellow"), 50);
    +    println!("{:?}", scores);
    +
    +    let teams = vec![String::from("Blue"), String::from("Yellow")];
    +    let initial_scores = vec![10, 50];
    +    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
    +    println!("{:?}", scores);
    +
    +    let text = "hello world wonderful world";
    +    let mut map = HashMap::new();
    +    for word in text.split_whitespace() {
    +        let count = map.entry(word).or_insert(0);
    +        *count += 1;
    +    }
    +    println!("{:?}", map);
    +}
    +
    +enum Cell {
    +    Int(i32),
    +    Float(f64),
    +    Text(String),
     }

--- collection/src/main.rs
+++ collection/src/main.rs
@@ -1,3 +1,56 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::new();
+    let data = "inital contents";
+    let s = data.to_string();
+    println!("{}", s);
+    let s = "inital contents".to_string();
+    println!("{}", s);
+    let s = String::from("inital contents");
+    println!("{}", s);
+
+    let mut s = String::from("foo");
+    println!("{}", s);
+    s.push_str("bar");
+    println!("{}", s);
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {}", s2);
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = format!("{}-{}-{}", s1, s2, s3);
+    println!("s is {}", s);
+
+    for c in "नमस्ते".chars() {
+        println!("{}", c);
+    }
+
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    println!("{:?}", scores);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+    println!("{:?}", scores);
+
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", map);
+}
+
+enum Cell {
+    Int(i32),
+    Float(f64),
+    Text(String),
 }
