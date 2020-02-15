c75d55805aa1858ed26543ee18495f9c4765c13a
hasefumi23 <hasefumi23@gmail.com>
2020-02-15 19:05:39 +0900

    [from now] 2020/02/15 19:05:39
    diff --git a/error/src/main.rs b/error/src/main.rs
    index e7a11a9..e461bc3 100644
    --- a/error/src/main.rs
    +++ b/error/src/main.rs
    @@ -1,3 +1,59 @@
    +use std::fs::File;
    +use std::io;
    +use std::io::Read;
    +use std::io::ErrorKind;
    +
     fn main() {
    -    println!("Hello, world!");
    +    let mut s = username_from_file();
    +    println!("{:?}", s);
    +}
    +
    +fn username_from_file() -> Result<String, io::Error> {
    +    let mut s = String::new();
    +    File::open("hello.txt")?.read_to_string(&mut s)?;
    +    Ok(s)
    +}
    +
    +fn username_from_file2() -> Result<String, io::Error> {
    +    let mut f = File::open("hello.txt")?;
    +    let mut s = String::new();
    +    f.read_to_string(&mut s)?;
    +    Ok(s)
    +}
    +
    +fn username_from_file1() -> Result<String, io::Error> {
    +    let f = File::open("hello.txt");
    +
    +    let mut f = match f {
    +        Ok(file) => file,
    +        Err(e) => return Err(e),
    +    };
    +
    +    let mut s = String::new();
    +    match f.read_to_string(&mut s) {
    +        Ok(_) => Ok(s),
    +        Err(e) => Err(e),
    +    }
    +}
    +
    +fn main2() {
    +    // let f = File::open("hello.txt").unwrap();
    +    let f = File::open("hello.txt").expect("Failed to open");
    +}
    +
    +fn main1() {
    +    let f = match File::open("hello.txt") {
    +        Ok(file) => file,
    +        Err(ref error) if error.kind() == ErrorKind::NotFound => {
    +            match File::create("hello.txt") {
    +                Ok(fc) => fc,
    +                Err(e) => {
    +                    panic!("Tried {:?}", e);
    +                },
    +            }
    +        },
    +        Err(error) => {
    +            panic!("panic {:?}", error);
    +        }
    +    };
     }

--- error/src/main.rs
+++ error/src/main.rs
@@ -1,3 +1,59 @@
+use std::fs::File;
+use std::io;
+use std::io::Read;
+use std::io::ErrorKind;
+
 fn main() {
-    println!("Hello, world!");
+    let mut s = username_from_file();
+    println!("{:?}", s);
+}
+
+fn username_from_file() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn username_from_file2() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn username_from_file1() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+fn main2() {
+    // let f = File::open("hello.txt").unwrap();
+    let f = File::open("hello.txt").expect("Failed to open");
+}
+
+fn main1() {
+    let f = match File::open("hello.txt") {
+        Ok(file) => file,
+        Err(ref error) if error.kind() == ErrorKind::NotFound => {
+            match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => {
+                    panic!("Tried {:?}", e);
+                },
+            }
+        },
+        Err(error) => {
+            panic!("panic {:?}", error);
+        }
+    };
 }
