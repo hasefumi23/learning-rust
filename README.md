e38f89c5ce7b8409b7230a532ac40f01c9244dc6
hasefumi23 <hasefumi23@gmail.com>
2020-02-17 22:03:33 +0900

    [from now] 2020/02/17 22:03:33
    diff --git a/README.md b/README.md
    index 5f0a4a1..e836230 100644
    --- a/README.md
    +++ b/README.md
    @@ -6,3 +6,11 @@
     - from here
       - https://doc.rust-jp.rs/book/second-edition/ch10-00-generics.html
    
    +```rust
    +fn largest<T>(list: &[T]) -> T {
    +```
    +
    +- この定義は以下のように解読します
    +  - 関数largestは、なんらかの型Tに関してジェネリックであると
    +  - この関数にはlistという引数が1つあり、これは型Tの値のスライスです
    +  - largest関数は同じT型の値を返します
    diff --git a/generics/src/main.rs b/generics/src/main.rs
    index 3d5852d..ba6a9e0 100644
    --- a/generics/src/main.rs
    +++ b/generics/src/main.rs
    @@ -9,7 +9,64 @@ fn largest(list: &[i32]) -> i32 {
         largest
     }
    
    +fn largest_i32(list: &[i32]) -> i32 {
    +    let mut largest = list[0];
    +    for &item in list.iter() {
    +        if item > largest {
    +            largest = item;
    +        }
    +    }
    +
    +    largest
    +}
    +
    +fn largest_char(list: &[char]) -> char {
    +    let mut largest = list[0];
    +    for &item in list.iter() {
    +        if item > largest {
    +            largest = item;
    +        }
    +    }
    +    largest
    +}
    +
    +// fn largest1<T>(list: &[T]) -> T {
    +//     let mut largest = list[0];
    +//     for &item in list.iter() {
    +//         if item > largest {
    +//             largest = item;
    +//         }
    +//     }
    +//     largest
    +// }
    +
    +// struct Point<T> {
    +//     x: T,
    +//     y: T,
    +// }
    +
    +struct Point<T, U> {
    +    x: T,
    +    y: U,
    +}
    +
     fn main() {
    +    let integer = Point { x: 5, y: 10 };
    +    let float = Point { x: 1.0, y: 4.0 };
    +    let wont_work = Point { x: 1, y: 4.0 };
    +}
    +
    +fn main2() {
    +    let nubmer_list = vec![34, 50, 25, 100, 65];
    +    let result = largest_i32(&nubmer_list);
    +    println!("The is {}", result);
    +
    +    let char_list = vec!['y', 'm', 'a', 'q'];
    +    let result = largest_char(&char_list);
    +    println!("The is {}", result);
    +}
    +
    +fn main1() {
         let nubmer_list = vec![34, 50, 25, 100, 65];
         let result = largest(&nubmer_list);
         println!("The is {}", result);

--- README.md
+++ README.md
@@ -6,3 +6,11 @@
 - from here
   - https://doc.rust-jp.rs/book/second-edition/ch10-00-generics.html
 
+```rust
+fn largest<T>(list: &[T]) -> T {
+```
+
+- この定義は以下のように解読します
+  - 関数largestは、なんらかの型Tに関してジェネリックであると
+  - この関数にはlistという引数が1つあり、これは型Tの値のスライスです
+  - largest関数は同じT型の値を返します
--- generics/src/main.rs
+++ generics/src/main.rs
@@ -9,7 +9,64 @@ fn largest(list: &[i32]) -> i32 {
     largest
 }
 
+fn largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+// fn largest1<T>(list: &[T]) -> T {
+//     let mut largest = list[0];
+//     for &item in list.iter() {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
+
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
 fn main() {
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+    let wont_work = Point { x: 1, y: 4.0 };
+}
+
+fn main2() {
+    let nubmer_list = vec![34, 50, 25, 100, 65];
+    let result = largest_i32(&nubmer_list);
+    println!("The is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest_char(&char_list);
+    println!("The is {}", result);
+}
+
+fn main1() {
     let nubmer_list = vec![34, 50, 25, 100, 65];
     let result = largest(&nubmer_list);
     println!("The is {}", result);
