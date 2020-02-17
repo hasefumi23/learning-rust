4da10734e016016e4de7d42a38017ae4ac4ba086
hasefumi23 <hasefumi23@gmail.com>
2020-02-17 21:27:21 +0900

    [from now] 2020/02/17 21:27:20
    diff --git a/generics/src/main.rs b/generics/src/main.rs
    index e7a11a9..7b6918c 100644
    --- a/generics/src/main.rs
    +++ b/generics/src/main.rs
    @@ -1,3 +1,10 @@
     fn main() {
    -    println!("Hello, world!");
    +    let nubmer_list = vec![34, 50, 25, 100, 65];
    +    let mut largest = nubmer_list[0];
    +    for number in nubmer_list {
    +        if number > largest {
    +            largest = number;
    +        }
    +    }
    +    println!("The is {}", largest);
     }

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let nubmer_list = vec![34, 50, 25, 100, 65];
+    let mut largest = nubmer_list[0];
+    for number in nubmer_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+    println!("The is {}", largest);
 }
