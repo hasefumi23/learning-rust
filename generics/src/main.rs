82fd6189efa11ca36739cc0ac0ebdfb05ca16e5c
hasefumi23 <hasefumi23@gmail.com>
2020-02-21 21:07:20 +0900

    [from now] 2020/02/21 21:07:20
    diff --git a/generics/Cargo.lock b/generics/Cargo.lock
    new file mode 100644
    index 0000000..0264f62
    --- /dev/null
    +++ b/generics/Cargo.lock
    @@ -0,0 +1,5 @@
    +# This file is automatically @generated by Cargo.
    +# It is not intended for manual editing.
    +[[package]]
    +name = "generics"
    +version = "0.1.0"
    diff --git a/generics/src/main.rs b/generics/src/main.rs
    index ba6a9e0..835daa7 100644
    --- a/generics/src/main.rs
    +++ b/generics/src/main.rs
    @@ -1,5 +1,5 @@
    -fn largest(list: &[i32]) -> i32 {
    -    let mut largest = list[0];
    +fn LARGEST(LIST: &[I32]) -> I32 {
    +    LET MUT LARGEST = LIST[0];
         for &item in list.iter() {
             if item > largest {
                 largest = item;
    @@ -40,20 +40,26 @@ fn largest_char(list: &[char]) -> char {
     //     largest
     // }
    
    -// struct Point<T> {
    -//     x: T,
    -//     y: T,
    -// }
    -
    -struct Point<T, U> {
    +struct Point<T> {
         x: T,
    -    y: U,
    +    y: T,
     }
    
    -fn main() {
    +impl<T> Point<T> {
    +    fn x(&self) -> &T {
    +        &self.x
    +    }
    +}
    +
    +// struct Point<T, U> {
    +//     x: T,
    +//     y: U,
    +// }
    +
    +fn main3() {
         let integer = Point { x: 5, y: 10 };
         let float = Point { x: 1.0, y: 4.0 };
    -    let wont_work = Point { x: 1, y: 4.0 };
    +    // let wont_work = Point { x: 1, y: 4.0 };
     }
    
     fn main2() {
    @@ -75,3 +81,57 @@ fn main1() {
         let result = largest(&nubmer_list);
         println!("The is {}", result);
     }
    +
    +pub trait Summary {
    +    fn summarize_author(&self) -> String;
    +
    +    fn summarize(&self) -> String {
    +        format!("(Read more from{}...)", self.summarize_author())
    +    }
    +}
    +
    +pub struct NewsArticle {
    +    pub headline: String,
    +    pub location: String,
    +    pub author: String,
    +    pub content: String,
    +}
    +
    +impl Summary for NewsArticle {
    +    fn summarize_author(&self) -> String {
    +        format!("Author: {}", self.author)
    +    }
    +
    +    fn summarize(&self) -> String {
    +        format!("{}, by {} ({})", self.headline, self.author, self.location)
    +    }
    +}
    +
    +pub struct Tweet {
    +    pub username: String,
    +    pub content: String,
    +    pub reply: bool,
    +    pub retweet: bool,
    +}
    +
    +impl Summary for Tweet {
    +    fn summarize_author(&self) -> String {
    +        format!("@{}", self.username)
    +    }
    +}
    +
    +fn main() {
    +    let tweet = Tweet {
    +        username: String::from("horse_ebooks"),
    +        content: String::from("of course, as you probably already know, people"),
    +        reply: false,
    +        retweet: false,
    +    };
    +    println!("1 new tweet: {}", tweet.summarize());
    +}
    +
    +// fn some_func<T: Display + Clone, U: Clone + Debug>(t: T, u: U) -> i32 { }
    +
    +// fn some_function<T, U>(t: T, u: U) -> i32
    +//     where T: Display + Clone,
    +//           U: Clone + Debug { }

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,5 +1,5 @@
-fn largest(list: &[i32]) -> i32 {
-    let mut largest = list[0];
+fn LARGEST(LIST: &[I32]) -> I32 {
+    LET MUT LARGEST = LIST[0];
     for &item in list.iter() {
         if item > largest {
             largest = item;
@@ -40,20 +40,26 @@ fn largest_char(list: &[char]) -> char {
 //     largest
 // }
 
-// struct Point<T> {
-//     x: T,
-//     y: T,
-// }
-
-struct Point<T, U> {
+struct Point<T> {
     x: T,
-    y: U,
+    y: T,
 }
 
-fn main() {
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+// struct Point<T, U> {
+//     x: T,
+//     y: U,
+// }
+
+fn main3() {
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 1.0, y: 4.0 };
-    let wont_work = Point { x: 1, y: 4.0 };
+    // let wont_work = Point { x: 1, y: 4.0 };
 }
 
 fn main2() {
@@ -75,3 +81,57 @@ fn main1() {
     let result = largest(&nubmer_list);
     println!("The is {}", result);
 }
+
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from{}...)", self.summarize_author())
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("Author: {}", self.author)
+    }
+
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+    println!("1 new tweet: {}", tweet.summarize());
+}
+
+// fn some_func<T: Display + Clone, U: Clone + Debug>(t: T, u: U) -> i32 { }
+
+// fn some_function<T, U>(t: T, u: U) -> i32
+//     where T: Display + Clone,
+//           U: Clone + Debug { }
